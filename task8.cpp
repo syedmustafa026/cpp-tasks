f00a9d1a75dc963d044ab2956f9c501a23a812a4
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2022-01-02 03:01:40 -0800

    labtask8 completed

--- task8.cpp
+++ task8.cpp
@@ -1,1 +1,106 @@
-d
+// #include <iostream>
+// using namespace std;
+
+// void mustafa();
+
+// void mustafa()
+// {
+//     string name;
+//     cout << "Enter your name 10 times using function\n";
+//     cin >> name;
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         cout << name<<endl;
+//     }
+// }
+// int main()
+// {
+//     mustafa();
+// }
+
+// #include <iostream>
+// using namespace std;
+// void fun();
+
+// void fun()
+// {
+//   int n;
+//     double factor = 1;
+//     cout << "Enter number for Factorial you want \n";
+//     cin >> n;
+//     for (int i = 1; i <= n; ++i)
+//     {
+//         factor *= i;
+//     }
+//     cout << "Factorial number of " << n << " is " << factor;
+// }
+// int main()
+// {
+//     fun();
+// }
+
+// #include <iostream>
+// using namespace std;
+// void fun();
+
+// void fun(){
+
+//     int n, sum = 0;
+
+//     cout << "Enter a positive integer: ";
+//     cin >> n;
+
+//     for (int i = 1; i <= n; ++i) {
+//         sum += i;
+//     }
+
+//     cout << "Sum of all natural numbers = " << sum;
+
+// }
+// int main(){
+//     fun();
+// }
+
+// #include <iostream>
+// using namespace std;
+// int product();
+
+// int product(){
+// float a;
+// int b;
+// cout<<"Enter Two numbers for product \n";
+// cin>>a>>b;
+// cout<<"The product is "<< a*b;
+// }
+
+// int main(){
+//     product();
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// void max_three();
+
+// void max_three()
+// {
+//     float n1, n2, n3;
+//     cout << "Enter three numbers: ";
+//     cin >> n1 >> n2 >> n3;
+//     if (n1 >= n2 && n1 >= n3)
+//         cout << "Largest number: " << n1;
+
+//     else if (n2 >= n1 && n2 >= n3)
+//         cout << "Largest number: " << n2;
+
+//     else if (n3 >= n1 && n3 >= n2)
+//         cout << "Largest number: " << n3;
+        
+// }
+
+// int main()
+// {
+//     max_three();
+// }
+
+
